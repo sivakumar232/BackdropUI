9a333f8e8fb404820bf386a24009c9d124428a26
Sivakumar <sivakumarr3105@gmail.com>
2025-07-29 22:15:26 +0530

    fix

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {  FaGithub } from "react-icons/fa";
-import { FaMoon } from 'react-icons/fa';
 
 const Navbar = ({ theme }) => {
   const iconColor = theme === "dark" ? "text-white" : "text-black";
@@ -9,7 +8,6 @@ const Navbar = ({ theme }) => {
     <nav className="mb-20 flex items-center justify-between py-6 px-6 md:py-10 md:px-40">
 
       <div className="ml-auto flex gap-6 cursor-pointer text-2xl md:text-3xl">
-        <FaMoon className='' />
                 <a
       href="https://github.com/sivakumar232/BackdropUI"
       target="_blank"
